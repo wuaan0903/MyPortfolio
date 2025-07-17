5c5da17f5ec08de2c2a90a8d24039962df6c78dd
wuaan0903 <nminhquan933@gmail.com>
2025-07-18 01:35:28 +0700

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,7 +40,7 @@ const Projects = () => {
                   d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                 ></path>
               </svg>
-              <span class="text">Modern Info</span>
+              <span class="text">More Info</span>
               <span class="circle"></span>
               <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -69,7 +69,7 @@ const Projects = () => {
                   d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                 ></path>
               </svg>
-              <span class="text">Modern Info</span>
+              <span class="text">More Info</span>
               <span class="circle"></span>
               <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -101,7 +101,7 @@ const Projects = () => {
                   d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                 ></path>
               </svg>
-              <span class="text">Modern Info</span>
+              <span class="text">More Info</span>
               <span class="circle"></span>
               <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -131,7 +131,7 @@ const Projects = () => {
                   d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                 ></path>
               </svg>
-              <span class="text">Modern Info</span>
+              <span class="text">More Info</span>
               <span class="circle"></span>
               <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
                 <path
